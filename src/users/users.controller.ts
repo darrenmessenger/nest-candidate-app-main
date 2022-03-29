6bfadd80ff08d2984465e095a658f8d6ed9f5cd3
Darren Messenger <39158691+darrenmessenger@users.noreply.github.com>
2022-03-29 09:55:33 +0100

    Added functionality so that users are sorted correctly
    Added functionality so that the users are  sorted by firstName then by lastName. The array of strings is then returned.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from './interfaces/users.interface'; //User interface
+import { UsersService } from './users.service'; //User data
 
 @Controller('users')
 export class UsersController {
     @Get()
     getAllSorted(): string {
-        return 'getAllSorted returned...';
+        let sortedUsers: User[];
+        let usersService = new UsersService;
+        sortedUsers = usersService.getUsers();
+
+        // Sort the users by firstName then lastName
+        let sortedUsersArray = sortedUsers.sort(function (a,b){
+            if (a.firstName < b.firstName)
+                return -1;
+            else if (a.firstName == b.firstName)
+            {
+                if (a.lastName < b.lastName) //if the firstNames match then check the lastNames and sort accordingly
+                    return -1;
+                else if (a.lastName == b.lastName)
+                    return 0;
+                else
+                    return 1;
+            }else
+                return 1;
+        })
+        
+        return JSON.stringify(sortedUsersArray);  //return the users sorted by firstName then lastName
     }
 }
