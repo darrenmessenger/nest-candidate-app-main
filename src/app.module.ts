f6525449876df173abbd8c4373a6c50b950c95d4
Darren Messenger <39158691+darrenmessenger@users.noreply.github.com>
2022-03-29 09:05:41 +0100

    Controller issue resolved
    Fixed the unfinished state so that the endpoint is working now.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
